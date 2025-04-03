d4a68e1bce0dcad97d659b51566c2bb88c761905
Ramagiri Suraj kumar <ramagirisurajkumar@gmail.com>
2025-04-03 16:18:23 +0530

    added vercel json for rendering issue

--- src/components/TeamMember.jsx
+++ src/components/TeamMember.jsx
@@ -2,7 +2,7 @@ import { MapPin, Briefcase } from "lucide-react";
 
 export default function TeamMember({ name, role, address, occupation }) {
   return (
-    <div className="bg-white p-6 rounded-2xl shadow-md hover:shadow-xl transition-transform duration-300 transform hover:-translate-y-1 border border-gray-200 max-w-sm mx-auto ">
+    <div className="bg-white p-6 rounded-2xl shadow-md hover:shadow-xl transition-transform duration-300 transform hover:-translate-y-1 border border-gray-200 max-w-sm mx-auto">
       <h3 className="text-2xl font-bold text-[#1E3A8A] mb-1">{name}</h3>
       <p className="text-[#FFD700] font-medium text-sm mb-4 uppercase tracking-wide">{role}</p>
 
